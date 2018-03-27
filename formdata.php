b5513fbb325926b6586940e645d69233536f2b3e
Tochoniy Alexey <tochoniy_alexey@ukr.net>
2018-03-27 12:11:05 +0300

    Sait stroy

---
+++ formdata.php
@@ -0,0 +1,95 @@
+<? 
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="admin@site.ru";  // e-mail админа 
+ 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+ 
+$time=date("H:i"); // часы:минуты:секунды 
+ 
+$backurl="http://StroySait.loc/index.html";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['name']; 
+ 
+$email=$_POST['email']; 
+ 
+$text=$_POST['text']; 
+ 
+  
+ 
+// Проверяем валидность e-mail 
+ 
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ 
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else 
+ 
+ { 
+ 
+ 
+$text=" 
+ 
+ 
+<p>Имя: $name</p> 
+ 
+ 
+<p>E-mail: $email</p> 
+ 
+ 
+<p>Сообщение: $text</p> 
+ 
+ 
+"; 
+ 
+  
+ 
+ // Отправляем письмо админу  
+ 
+mail("$adminemail", "$date $time Сообщение 
+от $name", "$text"); 
+ 
+  
+ 
+// Сохраняем в базу данных 
+ 
+$f = fopen("message.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $name"); 
+ 
+fwrite($f,"\n $text "); 
+ 
+fwrite($f,"\n ---------------"); 
+ 
+fclose($f); 
+ 
+  
+ 
+// Выводим сообщение пользователю 
+ 
+print "<center><script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$text 
+ 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ } 
+ 
